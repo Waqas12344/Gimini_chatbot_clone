698134ebaee837bdeec939ddc6e50caac9741446
react-redux-project <wm635950@gmail.com>
2024-06-03 14:10:58 +0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SideBar from './components/SideBar/SideBar'
+import Main from './components/main/Main'
+import './App.css'
+function App() {
+
+  return (
+    <>
+    <SideBar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
